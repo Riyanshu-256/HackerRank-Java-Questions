842679b8bd439912f6f0a71a359bc60423499fef
Riyanshu Sharma <riyanshu062@gmail.com>
2025-08-07 14:55:14 +0530

    Java Subarray

---
+++ Day_04/Java_Subarray.java
@@ -0,0 +1,31 @@
+package Day_04;
+
+import java.util.Scanner;
+
+public class Java_Subarray {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // sc.close();
+
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+                if (sum < 0) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
