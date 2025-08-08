89d68fdef19119a283dc32d72e0f3848e7b00eb2
Riyanshu Sharma <riyanshu062@gmail.com>
2025-08-08 19:34:34 +0530

    Solved

---
+++ Day_05/P02_Java_Currency_Formatter.java
@@ -0,0 +1,30 @@
+package Day_05;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class P02_Java_Currency_Formatter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        
+        // Write your code here.
+        
+        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+
+        Locale indiaLocale = new Locale("en", "IN");
+        
+        String india = NumberFormat.getCurrencyInstance(indiaLocale).format(payment);
+
+        String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+
+        String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
